f7bc96e86e37461ab6acda487730b91f90b69327
joshua_wqj <504865348@qq.com>
2017-04-07 21:16:42 +0800

    init project

---
+++ app/src/main/java/com/joshua/craftsman/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.joshua.craftsman.activity;
+
+import android.os.Bundle;
+
+import com.joshua.craftsman.R;
+import com.joshua.craftsman.activity.core.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
