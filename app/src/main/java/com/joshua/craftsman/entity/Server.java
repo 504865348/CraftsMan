88863d20377cc7d00d769f5ea7e9b2d0a158d86f
niezziot <niezziot@163.com>
2017-05-15 19:37:26 +0800

    nzz

--- app/src/main/java/com/joshua/craftsman/entity/Server.java
+++ app/src/main/java/com/joshua/craftsman/entity/Server.java
@@ -1,12 +1,37 @@
 package com.joshua.craftsman.entity;
 
 /**
- * Created by nzz on 2017/5/6.
+ * Created by nzz on 2017/5/11.
+ * methods
  */
 
 public class Server {
     //public static final String SERVER_REMOTE = "http://10.10.7.96:8080/MainServlet";
     public static final String SERVER_REMOTE = "http://114.55.148.222:8080/GJ/MainServlet";
-    public static final String SERVER_LOGIN = "Login";
+    public static final String SERVER_SIGN = "Sign"; //注册
+    public static final String SERVER_LOGIN = "Login"; //登录
+    public static final String CAROUSEL_PIC = "CarouselPic"; //获取轮播图片
+    public static final String HOME_HOT_CRAFTSMAN = "HotCraftsman"; //首页-热门-大国工匠
+    public static final String HOME_HOT_SKILLS = "Skills"; //首页-热门-匠心独运
+    public static final String HOME_HOT_POLICY = "Policy"; //首页-热门-讲政策
+    public static final String HOME_HOT_LISTEN = "Listen"; //首页-热门-听专题
+    public static final String HOME_HOT_LOOK = "Look"; //首页-热门-看利器
+    public static final String HOME_RECOMMEND = "Recommend"; //首页-推荐
+    public static final String HOME_CLASSIFY = "Classify"; //首页-分类
+    public static final String HOME_CRAFTSMAN = "Craftsman"; //首页-工匠
+    public static final String HOME_SEARCH_ALBUM = "SearchAlbum"; //首页-搜索-专辑
+    public static final String HOME_SEARCH_QUESANS = "SearchQuesAns"; //首页-搜索-问答
+    public static final String HOME_SEARCH_CRAFTSMAN = "SearchCraftsman"; //首页-搜索-工匠
+    public static final String TOPLIST_HOT = "TopListHot"; //榜单-最火节目飙升榜
+    public static final String TOPLIST_MORE = "TopListMore"; //榜单-最多订阅经典榜
+    public static final String TOPLIST_PAY= "TopListPay"; //榜单-付费精品飙升榜
+    public static final String TOPLIST_CRAFTSMAN = "TopListCraftsman"; //榜单-最大国工匠榜
+    public static final String QUESANS_CLASSIFY = "QuesAnsClassify"; //问答-分类
+    public static final String QUESANS_HOTMAN = "QuesAnsHotMan"; //问答-超级红人
+    public static final String QUESANS_LIST = "QuesAnsList"; //问答-列表
+    public static final String FIND_CIRCLE = "FindCircle"; //发现-匠友圈动态
+    public static final String FIND_CIRCLE_FRIENDS = "FindFriends"; //发现-匠友圈-找匠友
+    public static final String MY_CRAFTSMAN = "MyCraftsman"; //我的(工匠用户)
+    public static final String MY_PUBLIC = "MyPublic"; //我的(普通用户)
 
 }
