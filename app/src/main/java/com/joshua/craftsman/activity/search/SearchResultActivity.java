1d4479795801446c6b0efb9c2c741c7b599ee7a5
Lister <514862896@qq.com>
2017-05-17 17:14:46 +0800

    chen 5-17 X

--- app/src/main/java/com/joshua/craftsman/activity/search/SearchResultActivity.java
+++ app/src/main/java/com/joshua/craftsman/activity/search/SearchResultActivity.java
@@ -1,6 +1,5 @@
 package com.joshua.craftsman.activity.search;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
