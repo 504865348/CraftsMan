556df6215f29242fe68ba3b8c312113d23fec051
Lister <514862896@qq.com>
2017-05-17 13:00:55 +0800

    chen 5-17

--- app/src/main/java/com/joshua/craftsman/activity/search/SearchActivity.java
+++ app/src/main/java/com/joshua/craftsman/activity/search/SearchActivity.java
@@ -1,9 +1,11 @@
 package com.joshua.craftsman.activity.search;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.joshua.craftsman.R;
 
@@ -12,26 +14,32 @@ import butterknife.ButterKnife;
 
 public class SearchActivity extends AppCompatActivity implements View.OnClickListener {
 
-    @BindView(R.id.search_interface_back) ImageView img_back;
+    @BindView(R.id.search_history_back) ImageView img_back;
+    @BindView(R.id.search_interface_search) TextView text_search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.search_interface);
-        ButterKnife.bind(this);
+        setContentView(R.layout.search);
 
+        ButterKnife.bind(this);
         setListener();
     }
 
     private void setListener() {
         img_back.setOnClickListener(this);
+        text_search.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.search_interface_back:
+            case R.id.search_history_back:
                 finish();
+                break;
+            case R.id.search_interface_search:
+                Intent intent = new Intent(this, SearchResultActivity.class);
+                startActivity(intent);
                 break;
         }
     }
